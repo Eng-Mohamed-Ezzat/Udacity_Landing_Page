13082214a6ce24e470160db26183cdc486e9d042
Eng.Mohamed Ezzat <47626512+Eng-Mohamed-Ezzat@users.noreply.github.com>
2024-10-30 17:46:43 +0300

    Final

--- js/app.js
+++ js/app.js
@@ -22,13 +22,18 @@
  * Define Global Variables
  * 
 */
-
+const sections = document.querySelectorAll('section');
+const navList = document.getElementById('navbar__list');
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (rect.top >= 0 && rect.top <= 200);
+}
 
 
 
@@ -39,13 +44,36 @@
 */
 
 // build the nav
-
+function buildNav() {
+    sections.forEach((section) => {
+        const navItem = document.createElement('li');
+        navItem.innerHTML = `<a href="#${section.id}" class="menu__link">${section.dataset.nav}</a>`;
+        navList.appendChild(navItem);
+    });
+}
 
 // Add class 'active' to section when near top of viewport
-
+function setActiveSection() {
+    sections.forEach((section) => {
+        const navLink = document.querySelector(`a[href="#${section.id}"]`);
+        if (isInViewport(section)) {
+            section.classList.add('active-section');
+            navLink.classList.add('active-link');
+        } else {
+            section.classList.remove('active-section');
+            navLink.classList.remove('active-link');
+        }
+    });
+}
 
 // Scroll to anchor ID using scrollTO event
-
+function scrollToSection(event) {
+    if (event.target.nodeName === 'A') {
+        event.preventDefault();
+        const targetSection = document.querySelector(event.target.getAttribute('href'));
+        targetSection.scrollIntoView({ behavior: 'smooth' });
+    }
+}
 
 /**
  * End Main Functions
@@ -54,9 +82,12 @@
 */
 
 // Build menu 
+document.addEventListener('DOMContentLoaded', buildNav);
 
 // Scroll to section on link click
+navList.addEventListener('click', scrollToSection);
 
 // Set sections as active
+document.addEventListener('scroll', setActiveSection);
 
 
